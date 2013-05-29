42e823653c9de2fba00a2fb7b8e93ce484f9626c
Murtuza <murtuzafirst@gmail.com>
2013-05-29 18:52:21 +0530

    basic numeral functionality

--- numeral.rb
+++ numeral.rb
@@ -5,17 +5,49 @@ class Fixnum
 		numeral_string = ""
 		sorted_numerals = RomanNumeral.mapping.sort{|a,b| b[1] <=> a[1]}
 		divider = sorted_numerals.first[1]
+		while number < divider
+			sorted_numerals.delete_at(0)
+			divider = sorted_numerals.first[1]
+		end
 		while (number/divider > 0) do 
+			puts "number is #{number} and divider is #{divider}"
 			numeral_string << RomanNumeral.mapping.invert[divider]
 			number -= divider
 			break if number == 0
 			while number < divider do
+				# a, number = to_name(number)
+				# numeral_string << a
 				sorted_numerals.delete_at(0)
 				divider = sorted_numerals.first[1]
 			end
 		end
 		return numeral_string
 	end
+
+	# def to_name(number)
+	# 	if number > 900
+	# 		puts " comes inside 900"
+	# 		number -= 900
+	# 		return "CM", number
+	# 	elsif number > 400
+	# 		number -= 400
+	# 		return "CD", number
+	# 	elsif number > 90
+	# 		number -= 90
+	# 		return "XC", number
+	# 	elsif number > 40
+	# 		number -= 40
+	# 		return "XL", number
+	# 	elsif number > 9 
+	# 		number -= 9
+	# 		return "IX", number			
+	# 	elsif number > 4
+	# 		number -= 4
+	# 		return "IV", number
+	# 	else
+	# 		return "", number
+	# 	end
+	# end
 end
 
 class RomanNumeral
@@ -26,6 +58,7 @@ class RomanNumeral
 		end
 	end
 end
+
 x = gets
 x = x.chomp.to_i
 puts x.to_numeral
